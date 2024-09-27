124d739a4b306c6592c3aacd6e09a344a869e5e8
YasminDaniele <127122204+YasminDaniele@users.noreply.github.com>
2024-09-27 11:24:21 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+document.querySelector('.parametro-senha_botao')
+const numeroSenha = document.querySelector('.parametro-senha_texto'); 
+let numeroSenha = 12; 
+const botoes = document.querySelectorAll('.parametro-senha_botao');
+botoes[0].onclick = diminuiTamanho; 
+
+\\ código omitido 
+
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+geraSenha();
+
+function geraSenha (){
+    for (let i = 0; i < tamanhoSenha;i++) {
+let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+numeroAleatorio = Math.floor(numeroAleatorio);
+console.log( letrasMaiusculas[numeroAleatorio]);
+    }
+}
+campoSenha.value = letrasMaiusculas; 
+
+function diminuiTamanho(){
+    if (tamanhoSenha > 1) {
+\\ tamanhoSenha = tamanhoSenha - 1;
+ numeroSenha.textContent = tamanhoSenha
+}
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function aumentaTamanho() {
+    if (tamanhoSenha < 20) {
+        tamanhoSenha = tamanhoSenha + 1;
+     }
+    numeroSenha.textContent = tamanhoSenha; 
+}
