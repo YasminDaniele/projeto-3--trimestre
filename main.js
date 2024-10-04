6a5aa392ae8df7e047b6e790be26eaa8ba068bbe
YasminDaniele <127122204+YasminDaniele@users.noreply.github.com>
2024-10-04 08:16:25 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -8,10 +8,31 @@ botoes[0].onclick = diminuiTamanho;
 
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
+const numeros = '0123456789';
+const simbolos ='!@%*?';
 geraSenha();
+const checkbox = document.querySelectorAll('.checkbox');
+for(i=0; i < checkbox.length;i++){}
+console.log(checkbox[0].checked);
 
 function geraSenha (){
-    let senha = " ";
+    let alfabeto = '';
+    if (checkbox[0].checked){
+        alfabeto= alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked){
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked){
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked){
+        alfabeto = alfabeto + simbolos;
+    }
+    console.log(alfabeto);
+
+    let senha = ' ';
     for (let i = 0; i < tamanhoSenha;i++) {
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
